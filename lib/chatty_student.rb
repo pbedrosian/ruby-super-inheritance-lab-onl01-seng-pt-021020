4cdce149b6dd78c44eb86f371fb5233298296d44
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-12 01:18:29 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,10 +4,12 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
-  
+
   def raise_hand
     n = "Pick me!"
     puts n * 10
   end
+
+  super
 
 end
